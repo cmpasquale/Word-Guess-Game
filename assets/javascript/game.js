cfb0fcf9320d53467850e49fe48581a4cd67608e
Cecilia Pasquale <cecilia.pasquale16@gmail.com>
2019-02-24 16:36:49 -0500

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,73 +1,11 @@
-var wordGuess = ["lisa", "mo", "apu","krusty", "milhouse", "smithers", "flanders", "barney", "duffman","ralph","willie"];
-var cpuPick = "";
-var letterNumber = [];
-var letterSpaces = 0;
-var remaining = [];
-var wrongGuess = [];
-var playerGuess= " ";
-var wins = 0 ;
-var loses = 0 ;
-var guesses = 6;
+// Display Directions
+// Computer picks a random word from Array
+// Number of blanks is displayed(depends on the word)
+// user selects a letter to choose
+// once a letter is chosen it cannot be picked again 
+// winner has 6 tries to guess the word
+// if they win a message is displayed
+// if the lose a message is displayed
 
-function gameTime () {
-  guesses = 6;
-  cpuPick = wordGuess[Math.floor(Math.random() * wordGuess.length)];
-  letterNumber = cpuPick.split("");
-  letterSpaces = letterNumber.length;
-  console.log (cpuPick);
-  remaining = [];
-  loses = [];
-    for (var i = 0; i < letterSpaces.length; i++){
-      remaining.push("_");
-    }
-  document.getElementById("remaining").innerHTML = guesses;
-  document.getElementById("slots").innerHTML = remaining.join(" ");
-  document.getElementById("loses").innerHTML = loses.join(" ");
-}
-
-function checker(correctLetter){
-  var letterPresent = false;
-  for (var i = 0; i < letterSpaces.length ; i++){
-
-      if (cpuPick[i] === correctLetter) {
-          letterPresent = true;
-      }
-  }
-      if (letterPresent){
-        //differnet letter for variable because they are in the same function?
-        for (var a = 0; a < letterSpaces.length; a++){
-
-            if (cpuPick[a] === correctLetter){
-                remianing [a] = correctLetter;
-            }
-        }
-      }
-        else {
-          wrongGuess = [];
-          guesses--;
-        }
-      }
-
-function endRound(){
-  document.getElementById("remaining").innerHTML = guesses;
-  document.getElementById("slots").innerHTML = remaining.join(" ");
-  document.getElementById("loses").innerHTML = loses.join(" ");
-  if (letterNumber.toString() === remaining.toString() ){
-    wins ++;
-    alert ("You found the culprit!!");
-    document.getElementById("RoundWins").innerHTML = wins;
-    gameTime();
-  }
-  else if (guesses === 0) {
-    loses ++;
-    alert ("YOU LOSE! THEY GOT AWAY!");
-    document.getElementById("loses").innerHTML = wins;
-    gameTime();
-    }
-}
-  gameTime ();
-  document.onkeyup = function (event) {
-    playerGuess = ""; String.fromCharCode(event.which).toLowerCase();
-    checker(playerGuess);
-  };
-  endRound();
+var words = ['']
+var
